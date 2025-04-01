266b5caf3a78fb9b14eb3a02f3fa89a03fc3553d
DrecQ <drecquist@gmail.com>
2025-04-01 18:56:25 +0200

    Design de base du projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; // Assure-toi que le fichier CSS est bien importé
+import Home from './components/Layout/Home';
+import TodoApp from './components/Layout/SideBar';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoApp/>
+      <Home />
     </div>
   );
-}
+};
 
 export default App;
